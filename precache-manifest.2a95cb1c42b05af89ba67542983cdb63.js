382e72f597a262dddb7af224db83cf762f307bf2
Jean Dias <jean.dias@agencian1.com.br>
2018-12-15 06:01:59 -0200

    Updates

---
+++ precache-manifest.2a95cb1c42b05af89ba67542983cdb63.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/star-challenge/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "01040bfc6c59f74c4fb9",
+    "url": "/star-challenge/static/js/runtime~main.01040bfc.js"
+  },
+  {
+    "revision": "6447b882faa9d4c60012",
+    "url": "/star-challenge/static/js/main.6447b882.chunk.js"
+  },
+  {
+    "revision": "82dafdb578ba27673946",
+    "url": "/star-challenge/static/js/1.82dafdb5.chunk.js"
+  },
+  {
+    "revision": "6447b882faa9d4c60012",
+    "url": "/star-challenge/static/css/main.b83543f1.chunk.css"
+  },
+  {
+    "revision": "72a5ded773081edba38fac583489606d",
+    "url": "/star-challenge/index.html"
+  }
+];
